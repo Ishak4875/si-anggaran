9a065c4a2e23e890fe399bcebf574a5198ea8878
Ishak4875 <ishakjaelani07@gmail.com>
2026-08-19 15:03:24 +0800

    refactor: update PacketsRegularSeeder to match Excel data with API packets by nama_paket
    - Extracted 30 packages from Excel file (D:\08-13 Daftar Paket Konstruksi.xlsx)
    - Matched packages to packets table using fuzzy name matching by satker
    - Used actual kdpaket codes from API for matched packages
    - Generated unique codes (OPA-NNN format) for packages not found in API
    - All packages now have nama_paket synced from Excel for consistency
    - Pagu values pulled from matched packets in packets table
    
    Summary:
    - balai: 5 packages (all matched from API)
    - op: 7 packages (generated codes, awaiting API sync)
    - pjsa: 16 packages (all matched from API)
    - bendungan: 2 packages (all matched from API)
    - Total: 30 packages

--- database/seeders/PacketsRegularSeeder.php
+++ database/seeders/PacketsRegularSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\PacketsRegular;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PacketsRegularSeeder extends Seeder
@@ -12,188 +11,218 @@ class PacketsRegularSeeder extends Seeder
     {
         PacketsRegular::truncate();
 
-        $data = [
-            [
-                'kode_paket' => '03.694170.FC.7691.CBR.001.301.A',
-                'nama_paket' => 'Desain Rehabilitasi Jaringan Irigasi DI. Mowila Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'balai',
-                'pagu' => 1602796000,
-            ],
-            [
-                'kode_paket' => '03.694170.FC.7691.CBR.001.301.B',
-                'nama_paket' => 'Pemutakhiran Pemetaan Daerah Irigasi Kewenangan Pusat di Provinsi Sulawesi Tenggara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'balai',
-                'pagu' => 1764411000,
-            ],
-            [
-                'kode_paket' => '03.694170.FC.7691.CBR.001.301.C',
-                'nama_paket' => 'Penyusunan Dokumen Kesiapan Peningkatan Jaringan Irigasi DI. Watumokala Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'balai',
-                'pagu' => 1597768000,
-            ],
-            [
-                'kode_paket' => '03.694170.FC.7692.CBR.001.301.A',
-                'nama_paket' => 'Penyusunan Dokumen Lingkungan Kegiatan Pembangunan Pengaman Pantai Kawasan Anaiwoi - Kampung Bajo Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'balai',
-                'pagu' => 846600000,
-            ],
-            [
-                'kode_paket' => '03.694170.FC.7694.CBR.001.301.A',
-                'nama_paket' => 'Desain Rehabilitasi Air Baku Tolihe Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'balai',
-                'pagu' => 970140000,
-            ],
-            [
-                'kode_paket' => 'OPA-101',
-                'nama_paket' => 'Pembangunan Jaringan Irigasi DI Laiba Kab. Muna',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-102',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Jaringan Irigasi DI Laiba Kab. Muna',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-103',
-                'nama_paket' => 'Rehabilitasi Jaringan Irigasi D.I Walay Kab. Konawe (Tahap III)',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-104',
-                'nama_paket' => 'Supervisi Konstruksi Rehabilitasi Jaringan Irigasi D.I Walay Kab. Konawe (Tahap III)',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-105',
-                'nama_paket' => 'Rehabilitasi Jaringan Transmisi Air Baku Kota Baubau',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-106',
-                'nama_paket' => 'Supervisi Konstruksi Rehabilitasi Jaringan Transmisi Air Baku Kota Baubau',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'OPA-107',
-                'nama_paket' => 'Rehabilitasi Embung Ulu Benua Kab. Konawe',
-                'satker' => 'op',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.001.100.A',
-                'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Lasusua (Lanjutan) Kab. Kolaka Utara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 630461000,
-            ],
-            [
-                'kode_paket' => 'PJSA-101',
-                'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Lasusua (Lanjutan) Kab. Kolaka Utara',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'PJSA-102',
-                'nama_paket' => 'Pengendalian Banjir Sungai Lasolo (Lanjutan) Kab. Konawe Utara',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'PJSA-103',
-                'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Lasolo (Lanjutan) Kab. Konawe Utara',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.003.103.A',
-                'nama_paket' => 'Pembangunan Pengaman Pantai Lasusua (Lanjutan) Kab. Kolaka Utara; 0.5 Km; 5 Ha; F; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 16063925000,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.003.100.C',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Tondowolio (Lanjutan) Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 797865000,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.003.100.B',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Kawasan Anaiwoi-Kampung Bajo Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 518400000,
-            ],
-            [
-                'kode_paket' => 'PJSA-104',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Kawasan Anaiwoi-Kampung Bajo Kab. Kolaka',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'PJSA-105',
-                'nama_paket' => 'Pembangunan Pengaman Pantai Tondowolio (Lanjutan) Kab. Kolaka',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'PJSA-106',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Tondowolio (Lanjutan) Kab. Kolaka',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.003.103.D',
-                'nama_paket' => 'Pembangunan Pengaman Pantai Raha (Lanjutan-Tahap III) Kab. Muna; 0.475 Km; 4.75 Ha; F; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 16413438000,
-            ],
-            [
-                'kode_paket' => 'PJSA-107',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Raha (Lanjutan-Tahap III) Kab. Muna',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.RBS.005.100.A',
-                'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Konaweha Kab. Konawe; 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 599210000,
-            ],
-            [
-                'kode_paket' => 'PJSA-108',
-                'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Konaweha Kab. Konawe',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => 'PJSA-109',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Tanggul Banjir Sungai Wanggu Kota Kendari (Lanjutan)',
-                'satker' => 'pjsa',
-                'pagu' => 0,
-            ],
-            [
-                'kode_paket' => '03.694243.FC.7692.CBS.001.100.C',
-                'nama_paket' => 'Supervisi Konstruksi Pembangunan Tanggul Banjir Sungai Wanggu Kota Kendari (Lanjutan); 1 Dokumen; 1 Dokumen; NF; K; SYC',
-                'satker' => 'pjsa',
-                'pagu' => 500000000,
-            ],
-            [
-                'kode_paket' => '03.694143.FC.7693.CBG.001.100.A',
-                'nama_paket' => 'Supervisi Konstruksi Peningkatan Fungsi Tampungan Bendungan Ladongi untuk mendukung Ketahanan Pangan di Kabupaten Kolaka Timur; Sulawesi Tenggara; Kab. Kolaka Timur; 1 Dokumen; 1 Dokumen ; NF; K; SYC',
-                'satker' => 'bendungan',
-                'pagu' => 754247000,
-            ],
-            [
-                'kode_paket' => 'BND-101',
-                'nama_paket' => 'Supervisi Konstruksi Peningkatan Fungsi Tampungan Bendungan Ladongi untuk mendukung Ketahanan Pangan di Kabupaten Kolaka Timur',
-                'satker' => 'bendungan',
-                'pagu' => 0,
-            ],
-        ];
+        $data = array (
+  0 =>
+  array (
+    'kode_paket' => '03.694170.FC.7691.CBR.001.301.A',
+    'nama_paket' => 'Desain Rehabilitasi Jaringan Irigasi DI. Mowila Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'balai',
+    'pagu' => 1602796000,
+  ),
+  1 =>
+  array (
+    'kode_paket' => '03.694170.FC.7691.CBR.001.301.B',
+    'nama_paket' => 'Pemutakhiran Pemetaan Daerah Irigasi Kewenangan Pusat di Provinsi Sulawesi Tenggara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'balai',
+    'pagu' => 1764411000,
+  ),
+  2 =>
+  array (
+    'kode_paket' => '03.694170.FC.7691.CBR.001.301.C',
+    'nama_paket' => 'Penyusunan Dokumen Kesiapan Peningkatan Jaringan Irigasi DI. Watumokala Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'balai',
+    'pagu' => 1597768000,
+  ),
+  3 =>
+  array (
+    'kode_paket' => '03.694170.FC.7692.CBR.001.301.A',
+    'nama_paket' => 'Penyusunan Dokumen Lingkungan Kegiatan Pembangunan Pengaman Pantai Kawasan Anaiwoi - Kampung Bajo Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'balai',
+    'pagu' => 846600000,
+  ),
+  4 =>
+  array (
+    'kode_paket' => '03.694170.FC.7694.CBR.001.301.A',
+    'nama_paket' => 'Desain Rehabilitasi Air Baku Tolihe Kab. Konawe Selatan; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'balai',
+    'pagu' => 970140000,
+  ),
+  5 =>
+  array (
+    'kode_paket' => 'OPA-001',
+    'nama_paket' => 'Pembangunan Jaringan Irigasi DI Laiba Kab. Muna; 2.07 Km; 156.52 Ha; F; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  6 =>
+  array (
+    'kode_paket' => 'OPA-002',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Jaringan Irigasi DI Laiba Kab. Muna; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  7 =>
+  array (
+    'kode_paket' => 'OPA-003',
+    'nama_paket' => 'Rehabilitasi Jaringan Irigasi D.I Walay Kab. Konawe (Tahap III); 8.995 Km; 1420 Ha; F; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  8 =>
+  array (
+    'kode_paket' => 'OPA-004',
+    'nama_paket' => 'Supervisi Konstruksi Rehabilitasi Jaringan Irigasi D.I Walay Kab. Konawe (Tahap III); 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  9 =>
+  array (
+    'kode_paket' => 'OPA-005',
+    'nama_paket' => 'Rehabilitasi Jaringan Transmisi Air Baku Kota Baubau; 4 Km; 0.05 M3/Detik; F; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  10 =>
+  array (
+    'kode_paket' => 'OPA-006',
+    'nama_paket' => 'Supervisi Konstruksi Rehabilitasi Jaringan Transmisi Air Baku Kota Baubau; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  11 =>
+  array (
+    'kode_paket' => 'OPA-007',
+    'nama_paket' => 'Rehabilitasi Embung Ulu Benua Kab. Konawe; 1 Unit; 0.0006 m3/detik; F; K; SYC',
+    'satker' => 'op',
+    'pagu' => 0,
+  ),
+  12 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.103.A',
+    'nama_paket' => 'Pengendalian Banjir Sungai Lasusua (Lanjutan) Kab. Kolaka Utara; 0.4000 Km; 4 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 12968144000,
+  ),
+  13 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.100.A',
+    'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Lasusua (Lanjutan) Kab. Kolaka Utara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 630461000,
+  ),
+  14 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.103.B',
+    'nama_paket' => 'Pengendalian Banjir Sungai Lasolo (Lanjutan) Kab. Konawe Utara; 0.1000 Km; 1 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 5475692000,
+  ),
+  15 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.100.B',
+    'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Lasolo (Lanjutan) Kab. Konawe Utara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 318194000,
+  ),
+  16 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.103.A',
+    'nama_paket' => 'Pembangunan Pengaman Pantai Lasusua (Lanjutan) Kab. Kolaka Utara; 0.5 Km; 5 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 16063925000,
+  ),
+  17 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.100.A',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Lasusua (Lanjutan) Kab. Kolaka Utara; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 764791000,
+  ),
+  18 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.103.B',
+    'nama_paket' => 'Pembangunan Pengaman Pantai Kawasan Anaiwoi-Kampung Bajo Kab. Kolaka; 0.35 Km; 3.5 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 9874189000,
+  ),
+  19 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.100.B',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Kawasan Anaiwoi-Kampung Bajo Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 518400000,
+  ),
+  20 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.103.C',
+    'nama_paket' => 'Pembangunan Pengaman Pantai Tondowolio (Lanjutan) Kab. Kolaka ; 0.58 Km; 5.8 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 17280000000,
+  ),
+  21 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.100.C',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Tondowolio (Lanjutan) Kab. Kolaka; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 797865000,
+  ),
+  22 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.103.D',
+    'nama_paket' => 'Pembangunan Pengaman Pantai Raha (Lanjutan-Tahap III) Kab. Muna; 0.475 Km; 4.75 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 16413438000,
+  ),
+  23 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.003.100.D',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Pengaman Pantai Raha (Lanjutan-Tahap III) Kab. Muna; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 798300000,
+  ),
+  24 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.RBS.005.103.A',
+    'nama_paket' => 'Pengendalian Banjir Sungai Konaweha Kab. Konawe; 0.3 Km; 3 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 11605988000,
+  ),
+  25 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.RBS.005.100.A',
+    'nama_paket' => 'Supervisi Konstruksi Pengendalian Banjir Sungai Konaweha Kab. Konawe; 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 599210000,
+  ),
+  26 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.100.C',
+    'nama_paket' => 'Supervisi Konstruksi Pembangunan Tanggul Banjir Sungai Wanggu Kota Kendari (Lanjutan); 1 Dokumen; 1 Dokumen; NF; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 500000000,
+  ),
+  27 =>
+  array (
+    'kode_paket' => '03.694243.FC.7692.CBS.001.103.C',
+    'nama_paket' => 'Pembangunan Tanggul Banjir Sungai Wanggu Kota Kendari (Lanjutan); 0.8000 Km; 8 Ha; F; K; SYC',
+    'satker' => 'pjsa',
+    'pagu' => 14500000000,
+  ),
+  28 =>
+  array (
+    'kode_paket' => '03.694143.FC.7693.CBG.001.105.A',
+    'nama_paket' => 'Peningkatan Fungsi Tampungan Bendungan Ladongi untuk mendukung Ketahanan Pangan di Kabupaten Kolaka Timur; 2 Unit; 0.02 Juta M3; F; K; SYC',
+    'satker' => 'bendungan',
+    'pagu' => 15544299000,
+  ),
+  29 =>
+  array (
+    'kode_paket' => '03.694143.FC.7693.CBG.001.100.A',
+    'nama_paket' => 'Supervisi Konstruksi Peningkatan Fungsi Tampungan Bendungan Ladongi untuk mendukung Ketahanan Pangan di Kabupaten Kolaka Timur; Sulawesi Tenggara; Kab. Kolaka Timur; 1 Dokumen; 1 Dokumen ; NF; K; SYC',
+    'satker' => 'bendungan',
+    'pagu' => 754247000,
+  ),
+);
 
         foreach ($data as $packet) {
             PacketsRegular::create($packet);
